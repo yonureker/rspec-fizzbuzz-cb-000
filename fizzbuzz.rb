5661253a726c60eca106cccc18f60d7afaca247f
Onur Eker <yonureker@gmail.com>
2017-12-07 07:15:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
-  if number % 3 == 0
+  if number % 3 == 0 && number % 5 != 0
     return "Fizz"
-  elsif number % 5 == 0
+  elsif number % 5 == 0 && number % 3 != 0
     return "Buzz"
   elsif number % 3 == 0 && number % 5 == 0
     return "FizzBuzz"
